bf8887f7ddfc7bcbf0d7a4eaf8c0596078a2a09b
Jay <jay.sanjay.karia@gmail.com>
2024-09-28 16:26:04 +0530

    chore: initial project setup

---
+++ eslint.config.js
@@ -0,0 +1,25 @@
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
+
+export default [
+    { files: ['**/*.{js,mjs,cjs,ts}'] },
+    { languageOptions: { globals: globals.browser } },
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommended,
+    {
+        rules: {
+            'no-undef': 'off',
+            'no-unused-vars': 'off',
+            'no-shadow': 'off',
+            'no-use-before-define': 'off',
+            'no-undef-init': 'off',
+            'no-undef': 'off',
+            'no-unused-expressions': 'off',
+            'no-unused-labels': 'off',
+            'no-unused-vars': 'off',
+            'no-unused-vars': 'off',
+            "camelcase": "error",
+        },
+    },
+]
---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+/**
+ * @type {import("prettier").Config}
+ */
+const config = {
+    trailingComma: 'es5',
+    tabWidth: 4,
+    semi: false,
+    singleQuote: true,
+}
+
+export default config
